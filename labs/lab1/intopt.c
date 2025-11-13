2a1a61f3a914ed39faccd99159d2af44269cb21b
lucas-mansson <lu5132ma-s@student.lu.se>
2025-11-13 11:17:14 +0100

    finish lab 1

--- labs/lab1/intopt.c
+++ labs/lab1/intopt.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double** make_matrix(int rows, int cols)
+double** alloc_matrix(int rows, int cols)
 {
         double** a;
-        int i;
 
         a = calloc(rows, sizeof(double*));
-        for (i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++) {
                 a[i] = calloc(cols, sizeof(double));
         }
         return a;
@@ -21,19 +20,55 @@ int main(int argc, char** argv)
         scanf("%d %d", &m, &n);
         printf("Rows: %d Cols: %d \n", m, n);
 
+        printf("max z = ");
+
         // then we have a vector with n number of c-coefficients
+        double* c = calloc(n, sizeof(double));
+        for (int i = 0; i < n; i++) {
+                if (i > 0) {
+                        printf(" + ");
+                }
+                scanf("%lf", &c[i]);
+                printf("%2.1lfx_%d", c[i], i);
+        }
+        printf("\n");
 
         // then we have a matrix with m rows and n cols
-        double** matrix = make_matrix(m, n);
+        double** matrix = alloc_matrix(m, n);
         for (int i = 0; i < m; i++) {
-                for (int j = 0; i < n; i++) {
-                        double a;
-                        scanf("%lf", &a);
-                        printf("%lf ", a);
+                for (int j = 0; j < n; j++) {
+                        scanf("%lf", &matrix[i][j]);
                 }
         }
 
         // last is a vector with m number of b-values
+        double* b = calloc(n, sizeof(double));
+        for (int i = 0; i < n; i++) {
+                scanf("%lf", &b[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                        if (j > 0) {
+                                printf(" + ");
+                        }
+                        printf("%2.1lfx_%d", matrix[i][j], j);
+                }
+                printf(" <= %2.1lf", b[i]);
+                printf("\n");
+        }
+
+        free(c);
+        c = NULL; // good practice to set pointers to null after free
+        for (int i = 0; i < m; i++) {
+                free(matrix[i]);
+                matrix[i] = NULL;
+        }
+        free(matrix);
+        c = NULL;
+
+        free(b);
+        b = NULL;
 
         return 0;
 }
